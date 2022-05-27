de430d0cb425f99fc287d4f251f4e83e85fd1b28
nyazadzhiev <nyazadzhiev@gmail.com>
2022-05-27 23:19:44 +0300

    Implemented CRUD for products brands and types

---
+++ server/controllers/routes/Products.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const products = express.Router()
+const cors = require('cors')
+
+const Product = require("../../models/Product")
+products.use(cors())
+
+products.post("addProduct", (req, res) => {
+    const productData = {
+        name: req.body.name,
+        color: req.body.color,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        type_id: req.body.type_id,
+        brand_id: req.body.brand_id
+    }
+
+    Product.create(productData)
+    .then(product => {
+        res.send(product)
+    })
+    .catch(err => {
+        res.json("error: " + err)
+    })
+})
+
+products.delete("/deleteProduct", (req, res) => {
+    Product.destroy({
+        attributes: ['id', 'name', 'color', 'price', 'quantity', 'type_id', 'date_on_creating', 'date_of_last_modified', 'brand_id'],
+        where: {
+            id: req.body.id
+        }
+    })
+    .then(user => {
+        if(user){
+            res.send(user)
+        }
+    })
+    .catch(err => {
+        res.json("error: " + err)
+    })
+})
+
+module.exports = products
