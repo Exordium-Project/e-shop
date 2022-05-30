9090370e231b6dd1a0143bf00a0d1190ef708db3
Petar Arabadzhiev <petararabadjiev334@gmail.com>
2022-05-30 22:53:17 +0300

    [EX-20] Made the component responsive and fixed style issues.

--- client/src/components/search-bar/search-bar.js
+++ client/src/components/search-bar/search-bar.js
@@ -1,46 +1,55 @@
 import {useState} from "react";
 import {StyledEngineProvider} from "@mui/material";
 import TextFiled from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
-import { styled } from '@mui/material/styles';
 
-import styles from './search-bar.scss';
+import './search-bar.scss';
 
 
-const SearchBar = () =>{
+const SearchBar = () => {
 
     const [category, setCategory] = useState(0);
 
-    const handleChange = (event) =>{
+    const handleChange = (event) => {
         setCategory(event.target.value);
     }
 
-    return(
-      <div className="searchBar">
-          <StyledEngineProvider injectFirst={true}>
-              <TextFiled id="input-with-icon-textfield" placeholder="Search for anything" InputProps={{
-                  startAdornment: (
-                      <InputAdornment position="start">
-                          <SearchIcon />
-                      </InputAdornment>
-                  ),
-              }}></TextFiled>
-              <Select value={category} onChange={handleChange}>
-                  <MenuItem value={0}>All categories</MenuItem>
-                  <MenuItem value={10}>To</MenuItem>
-                  <MenuItem value={20}>Be</MenuItem>
-                  <MenuItem value={30}>Fetched</MenuItem>
-                  <MenuItem value={40}>From</MenuItem>
-                  <MenuItem value={50}>Backend</MenuItem>
-              </Select>
-              <Button variant="outlined" size="large">Search</Button>
-          </StyledEngineProvider>
-      </div>
+    return (
+        <StyledEngineProvider injectFirst={true}>
+            <div className="searchBar">
+                <Grid container={true} spacing={0}>
+                    <Grid item={true} sm={7} xs={12}>
+                        <TextFiled className="textBox input"
+                                   placeholder="Search for anything" InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon/>
+                                </InputAdornment>
+                            ),
+                        }}></TextFiled>
+                    </Grid>
+                    <Grid item={true} sm={3} xs={12}>
+                        <Select className="input" value={category} onChange={handleChange}>
+                            <MenuItem value={0}>All categories</MenuItem>
+                            <MenuItem value={10}>To</MenuItem>
+                            <MenuItem value={20}>Be</MenuItem>
+                            <MenuItem value={30}>Fetched</MenuItem>
+                            <MenuItem value={40}>From</MenuItem>
+                            <MenuItem value={50}>Backend</MenuItem>
+                        </Select>
+                    </Grid>
+                    <Grid item={true} sm={2} xs={12}>
+                        <Button className="button" variant="outlined" size="large">Search</Button>
+                    </Grid>
+                </Grid>
+            </div>
+        </StyledEngineProvider>
     );
 }
 
-export default styled(SearchBar)(styles);
+export default SearchBar;
