73377c7bf058c70395541ac941900aa29e71ffc8
Ivaylo <ivailo.slavchev432@abv.bg>
2022-06-09 20:15:55 +0300

    side-bar updates

--- client/src/components/Header/Catalog/SideBar.js
+++ client/src/components/Header/Catalog/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Box, Typography, AppBar, Toolbar, Slider, Accordion, AccordionSummary, StyledEngineProvider, AccordionDetails, FormControl, FormControlLabel, RadioGroup, Radio } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import '../Styles/SideBar.scss'
@@ -9,137 +9,74 @@ const SideBar = () => {
     const updateValue = (e, data) => {
         setVal(data)
     }
-    const material = ['Space', 'Leather', 'Strickes']
-    const brands = ['Nike', 'Puma', 'Adidas']
-    const colors = ['Black', 'White', 'Red', 'Blue', 'Brown']
-    const cities = ['Sofia, Bulgaria', 'Plovdiv, Bulgaria', 'Varna, Bulgaria']
-    const leftSide = ['From 100$']
-    const rightSide = ['To 5000$']
 
+    const priceBoxes = [
+        { leftAndRightSide: 'From 100$' },
+        { leftAndRightSide: 'To 5000$' }
+    ]
+    const selectOptions = [
+        {
+            titles: 'Material',
+            firstOption: 'Space',
+            secondOption: 'Leather',
+        },
+        {
+            titles: 'Brands',
+            firstOption: 'Nike',
+            secondOption: 'Puma',
+        },
+        {
+            titles: 'Colors',
+            firstOption: 'Black',
+            secondOption: 'White'
+        },
+        {
+            titles: 'Characteristics',
+            firstOption: 'Sneakers',
+            secondOption: 'Athletic'
+        },
+        {
+            titles: 'Location',
+            firstOption: 'Sofia, Bulgaria',
+            secondOption: 'Plovdiv, Bulgaria'
+        }
+    ]
     return (
         <Box>
             <StyledEngineProvider injectFirst={true}>
                 <AppBar className='AppBar'>
                     <Typography className='price-txt'>Price</Typography>
+
                     <Toolbar>
                         <Box className='price-diapason'>
                             {
-                                leftSide.map(first => {
-                                    return <Typography className='price'>{first}</Typography>
-                                })
-                            }
-                            {
-                                rightSide.map(second => {
-                                    return <Typography className='price'>{second}</Typography>
-                                })
+                                priceBoxes.map(item => { return (<Typography className='price'>{item.leftAndRightSide}</Typography>) })
                             }
                         </Box>
                     </Toolbar>
                     <Slider className='slider' value={val} onChange={updateValue} />
+                    {
+                        selectOptions.map(item => (
+                            <Box className='side-bar-boxes'>
 
-                    <Box className='side-bar-boxes'>
-                        <Accordion className='acordion-style'>
-                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                                <Typography>Material</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>
-                                    <FormControl>
-                                        <RadioGroup aria-labelledby="demo-radio-buttons-group-label" name="radio-buttons-group">
-                                            {
-                                                material.map(item => { return <FormControlLabel label={item} value={item} control={<Radio />} /> })
-                                            }
-                                        </RadioGroup>
-                                    </FormControl>
-                                </Typography>
-                            </AccordionDetails>
-
-                        </Accordion>
-                    </Box>
-
-                    <Box className='side-bar-boxes'>
-                        <Accordion className='acordion-style'>
-                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                                <Typography>Brands</Typography>
-                            </AccordionSummary>
-
-                            <AccordionDetails>
-                                <Typography>
-                                    <FormControl>
-                                        <RadioGroup aria-labelledby="demo-radio-buttons-group-label" name="radio-buttons-group">
-                                            {
-                                                brands.map(brandsItems => { return <FormControlLabel label={brandsItems} value={brandsItems} control={<Radio />} /> })
-                                            }
-                                        </RadioGroup>
-                                    </FormControl>
-                                </Typography>
-                            </AccordionDetails>
-
-                        </Accordion>
-                    </Box>
-
-                    <Box className='side-bar-boxes'>
-                        <Accordion className='acordion-style'>
-                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                                <Typography>Color</Typography>
-                            </AccordionSummary>
-
-                            <AccordionDetails>
-                                <Typography>
-                                    <FormControl>
-                                        <RadioGroup aria-labelledby="demo-radio-buttons-group-label" name="radio-buttons-group" >
-                                            {
-                                                colors.map(colorItems => { return <FormControlLabel value={colorItems} label={colorItems} control={<Radio />} /> })
-                                            }
-                                        </RadioGroup>
-                                    </FormControl>
-                                </Typography>
-                            </AccordionDetails>
-
-                        </Accordion>
-
-                    </Box>
-                    <Box className='side-bar-boxes'>
-                        <Accordion className='acordion-style'>
-                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                                <Typography>Characters</Typography>
-                            </AccordionSummary>
-
-                            <AccordionDetails>
-                                <Typography>
-                                    <FormControl>
-                                        <RadioGroup aria-labelledby="demo-radio-buttons-group-label" name="radio-buttons-group">
-
-                                            <FormControlLabel value="Sneakers" control={<Radio />} label="Sneakers" />
-
-                                        </RadioGroup>
-                                    </FormControl>
-                                </Typography>
-                            </AccordionDetails>
-
-                        </Accordion>
-
-                    </Box>
-                    <Box className='side-bar-boxes'>
-                        <Accordion className='acordion-style'>
-                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                                <Typography>Locations</Typography>
-                            </AccordionSummary>
-
-                            <AccordionDetails>
-                                <Typography>
-                                    <FormControl>
-                                        <RadioGroup aria-labelledby="demo-radio-buttons-group-label" name="radio-buttons-group" >
-                                            {
-                                                cities.map(items => { return <FormControlLabel value={items} label={items} control={<Radio />} /> })
-                                            }
-                                        </RadioGroup>
-                                    </FormControl>
-                                </Typography>
-                            </AccordionDetails>
-                        </Accordion>
-
-                    </Box>
+                                <Accordion className='acordion-style'>
+                                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                                        <Typography>{item.titles}</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                        <Typography>
+                                            <FormControl>
+                                                <RadioGroup aria-labelledby="demo-radio-buttons-group-label" name="radio-buttons-group">
+                                                    <FormControlLabel label={item.firstOption} value={item.firstOption} control={<Radio />} />
+                                                    <FormControlLabel label={item.secondOption} value={item.secondOption} control={<Radio />} />
+                                                </RadioGroup>
+                                            </FormControl>
+                                        </Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                            </Box>
+                        ))
+                    }
                 </AppBar>
             </StyledEngineProvider>
         </Box>
