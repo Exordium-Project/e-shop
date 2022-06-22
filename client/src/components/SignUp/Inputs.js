29a6845f6a5ea589c2ce5601e509cf3399e6349c
Ivaylo <ivailo.slavchev432@abv.bg>
2022-06-22 18:11:34 +0300

    responsive sign-up page

--- client/src/components/SignUp/Inputs.js
+++ client/src/components/SignUp/Inputs.js
@@ -1,60 +1,71 @@
-import React from 'react'
+import { Typography, TextField, Grid, Box, Link, Button } from '@mui/material'
+import { useForm } from 'react-hook-form';
 import './SignUpStyles/Registration-inputs.scss'
 import './SignUpStyles/Mobile-phone-view.scss'
-import { StyledEngineProvider, Grid, Box, TextField, Link, Button } from '@mui/material'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GoogleIcon from '@mui/icons-material/Google';
 import AppleIcon from '@mui/icons-material/Apple';
-import { useForm } from 'react-hook-form'
 
-const Inputs = () => {
-    const {register, handleSubmit, formState: {errors}} = useForm()
+
+export default function SignUp() {
+    const { register, handleSubmit, formState: { errors } } = useForm()
     const onSubmit = (data) => console.log(data)
     return (
-        <StyledEngineProvider injectFirst={true}>
-            <Grid xs={7} md={7}>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Box className='name-box'>
-                        <TextField placeholder='First name' 
-                            {...register('firstName', {required: 'Required field'})}
+        <Box component="form" noValidate sx={{ mt: 3 }}>
+            <Box className='main-box'>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={6}>
+                        <TextField placeholder='First name' fullWidth
+                            {...register('firstName', { required: 'Required field' })}
                             error={!!errors?.firstName}
-                            helperText={errors?.firstName ? errors.firstName.message : null}
-                        />
-                        <TextField placeholder='Last name' 
-                            {...register('lastName', {required: 'Required field'})}
+                            helperText={errors?.firstName ? errors.firstName.message : null} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <TextField placeholder='Last name' fullWidth
+                            {...register('lastName', { required: 'Required field' })}
                             error={!!errors?.lastName}
-                            helperText={errors?.lastName ? errors.lastName.message : null}/>
-                    </Box>
-                    <Box className='email-box'>
-                            <TextField placeholder='Email' {...register('email', {required: 'This field is required', pattern: { 
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,   
-                            message: 'Invalid email address'}})}
+                            helperText={errors?.lastName ? errors.lastName.message : null} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField placeholder='Email' fullWidth {...register('email', {
+                            required: 'This field is required', pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: 'Invalid email address'
+                            }
+                        })}
                             error={!!errors?.email}
-                            helperText={errors?.email ? errors.email.message : null}
-                        />
-                    </Box>
-                    <Box className='password-box'>
-                        <TextField placeholder='Password' type='password' />
-                    </Box>
+                            helperText={errors?.email ? errors.email.message : null} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField required fullWidth placeholder='Password' type='password'
+                            {...register('password', {
+                                required: 'This field is required', pattern: {
+                                    value: /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+                                    message: 'Password should be at least 8 characters long'
+                                }
+                            })}
+                            error={!!errors?.password}
+                            helperText={errors?.password ? errors.password.message : null} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography className='tearms-and-conditions'>By signing in you accept Exordium Terms and Conditions <Link to="#" sx={{ cursor: 'pointer' }}>Learn more</Link></Typography>
+                    </Grid>
+                    <button onClick={handleSubmit(onSubmit)} className='create-account-button'>Create account</button>
+                </Grid>
 
-                    <Box className='span-and-button'>
-                        <span className='tearms-and-conditions'>By signing in you accepting Exordium Terms and Conditions <Link to='#' sx={{cursor: 'pointer'}}>Learn more</Link></span>
-                        <button className='button'>Create account</button>
-                    </Box>
-                </form>
                 <Box className='vertical-line'>
                     <Box className='vertical-line-text'>or</Box>
                 </Box>
-            </Grid>
-            <Grid xs={12} md={12}>
-                <Box className='buttons-group'>
-                    <Button className='facebook-signUp'><FacebookIcon className='facebook-icon' /> Facebook</Button>
-                    <Button className='google-signUp'><GoogleIcon className='google-icon' /> Continue with Google</Button>
-                    <Button className='apple-signUp'><AppleIcon className='apple-icon' /> Continue with Apple</Button>
-                </Box>
-            </Grid>
-        </StyledEngineProvider>
-    )
+                <Grid item xs={12} md={12}>
+                    <Box className='container'>
+                        <Box className='buttons-group'>
+                            <Button className='facebook-signUp'><FacebookIcon /> Facebook</Button>
+                            <Button className='google-signUp'><GoogleIcon /> Continue with Google</Button>
+                            <Button className='apple-signUp'><AppleIcon /> Continue with Apple</Button>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Box>
+        </Box>
+    );
 }
-
-export default Inputs
