969e114bd21445dbb36d3c60e2788a713fee167d
Medikko <mehmeddukov@gmail.com>
2022-07-06 23:29:18 +0300

    Fixed thrown error with removing of interpolation.

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,8 +1,8 @@
 import {Sequelize} from "sequelize"
 
 const db = {}
-const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
-    host: `${process.env.DB_HOST}`,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     operatorsAliases: false,
     timezone: '+02:00',
