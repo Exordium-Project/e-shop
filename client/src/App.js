25de7bcd8eeb4c24238840974915c6bf3c920c82
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2022-07-23 19:11:02 +0200

    i18n added

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,29 @@ import Main from './components/main-layout/mainPage';
 import './components/main-layout/mainPage.scss';
 import SignUp from './components/SignUp/SignUp';
 import SignIn from './components/SignIn/SignIn';
-
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-xhr-backend";
+import {en} from './i18n/en'
+import {bg} from './i18n/bg'
+i18n.use(Backend).use(initReactI18next).init({
+    resources: {
+        en: en,
+        bg: bg
+      },
+      lng: "bg", 
+      fallbackLng: "en",
+  
+      interpolation: {
+        escapeValue: false
+      }
+})
 
 function App() {
-
+    
     return (
         <div className="App">
-            <Header />
+            <Header/>
 
             <Routes>
                 <Route path='/' element={<Main />} />
